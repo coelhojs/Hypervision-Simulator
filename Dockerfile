298d3104a743ee2714c63a209dab5b81ec0800ec
Cristiano Coelho de Jesus Souza <cristiano.souza@concert.com.br>
2020-10-29 11:31:34 -0300

    Criação de Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
+#PARA BUILD:
+#docker build . -t kafka-rest-simulator
+#PARA EXECUTAR [-p PORTA_HOST:PORTA_CONTAINER]:
+#docker run -p 5090:80 kafka-rest-simulator --restart=on-failure:10 --ip 192.168.2.93 --port 8082 --option 1 --count 10 --seconds 5
+
 #See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
 
 FROM mcr.microsoft.com/dotnet/core/runtime:3.1-buster-slim AS base
@@ -6,6 +11,7 @@ WORKDIR /app
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster AS build
 WORKDIR /src
 COPY ["Kafka-REST-Simulator/Simulator.csproj", "Kafka-REST-Simulator/"]
+COPY ["commons-core/Commons-Core.csproj", "commons-core/"]
 RUN dotnet restore "Kafka-REST-Simulator/Simulator.csproj"
 COPY . .
 WORKDIR "/src/Kafka-REST-Simulator"
@@ -17,4 +23,10 @@ RUN dotnet publish "Simulator.csproj" -c Release -o /app/publish
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
+#ENV ip=${""}
+#ENV port=${""}
+#ENV option=${""}
+#ENV count=${""}
+#ENV seconds=${""}
 ENTRYPOINT ["dotnet", "Simulator.dll"]
+#ENTRYPOINT ["dotnet", "Simulator.dll", "--ip ${ip}", "--port ${port}", "--option ${option}", "--count ${count}", "--seconds ${seconds}"]
--- Kafka-REST-Simulator/Program.cs
+++ Kafka-REST-Simulator/Program.cs
@@ -10,8 +10,12 @@ namespace KafkaRESTSimulator
 {
     public class Program
     {
+        private static int Interval;
+        private static int Iterations;
+        private static int Option;
         private static string RESTProxyIP;
         private static string RESTProxyPort;
+
         public static IConfigurationRoot Configuration { get; set; }
 
         public static async Task Main(string[] args)
@@ -22,6 +26,8 @@ namespace KafkaRESTSimulator
 
                 if (args.Length > 0)
                 {
+                    Console.WriteLine(string.Join(", ", args));
+
                     for (int i = 0; i < args.Length; i++)
                     {
                         string value = args[i];
@@ -33,6 +39,15 @@ namespace KafkaRESTSimulator
                                 break;
                             case "--port":
                                 RESTProxyPort = args[i + 1];
+                                break;
+                            case "--option":
+                                Option = Convert.ToInt32(args[i + 1]);
+                                break;
+                            case "--count":
+                                Iterations = Convert.ToInt32(args[i + 1]);
+                                break;
+                            case "--seconds":
+                                Interval = Convert.ToInt32(args[i + 1]);
                                 break;
                         }
                     }
@@ -46,29 +61,29 @@ namespace KafkaRESTSimulator
                         RESTProxyIP = (string)appSettings["RESTProxyIP"];
                         RESTProxyPort = (string)appSettings["RESTProxyPort"];
                     }
+
+                    Console.WriteLine("Selecione o tópico e pressione Enter:");
+                    Console.WriteLine("1-Meteorological-Alerts + Fire-Alerts\n");
+                    Console.WriteLine("2-Meteorological-Alerts\n");
+                    Console.WriteLine("3-Fire-Alerts\n");
+                    Option = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Digite a quantidade de mensagens a serem geradas\n");
+                    Iterations = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Digite o intervalo em segundos entre o envio de mensagens:\n");
+                    Interval = Convert.ToInt32(Console.ReadLine()) * 1000;
                 }
-
-                Console.WriteLine("Selecione o tópico e pressione Enter:");
-                Console.WriteLine("1-Meteorological-Alerts + Fire-Alerts\n");
-                Console.WriteLine("2-Meteorological-Alerts\n");
-                Console.WriteLine("3-Fire-Alerts\n");
-                var option = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Digite a quantidade de mensagens a serem geradas\n");
-                var iterations = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Digite o intervalo em segundos entre o envio de mensagens:\n");
-                var interval = Convert.ToInt32(Console.ReadLine()) * 1000;
 
                 var FireAlertPublisher = new Publisher(RESTProxyIP, RESTProxyPort, "Fire-Alerts");
                 var MeteorologicalAlertPublisher = new Publisher(RESTProxyIP, RESTProxyPort, "Meteorological-Alerts");
 
-                for (int i = 0; i < iterations; i++)
+                for (int i = 0; i < Iterations; i++)
                 {
                     var fireAlert = new FireAlert().Json;
                     var meteorologicalAlert = new MeteorologicalAlert().Json;
 
-                    switch (option)
+                    switch (Option)
                     {
                         case 1:
                             FireAlertPublisher.SendMessage(fireAlert);
@@ -85,7 +100,7 @@ namespace KafkaRESTSimulator
                             break;
                     }
 
-                    Thread.Sleep(interval);
+                    Thread.Sleep(Interval);
                 }
             }
             catch (Exception ex)
